da73fe6a05541ed158ca4267d12f5e91e6ee4a97
kshitij <albatros.29kr@gmail.com>
2024-05-20 13:20:54 +0530

    First Commit

---
+++ app/src/main/java/com/nextin/firstfirebaseapplication/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nextin.firstfirebaseapplication
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
+import com.nextin.firstfirebaseapplication.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding :ActivityMainBinding? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef1 = database.getReference("message")
+        val myRef2 = database.getReference("id")
+        val myRef3 = database.getReference("username")
+        myRef1.setValue("Hello, World!")
+        myRef2.setValue("Ak47")
+        myRef3.setValue("kshitij26")
+
+        myRef3.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue<String>()
+
+                binding?.tvMessage?.text = value
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "Failed to read value", error.toException())
+            }
+        })
+    }
+}
